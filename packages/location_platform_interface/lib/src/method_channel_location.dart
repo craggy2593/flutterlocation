8c955b6a57aa4cf7bdc3ea08376fc33807e5a180
Craig Rees <craig.rees@iblocks.co.uk>
2024-03-12 15:25:28 +0000

    Add foregroubd

--- packages/location_platform_interface/lib/src/method_channel_location.dart
+++ packages/location_platform_interface/lib/src/method_channel_location.dart
@@ -64,10 +64,13 @@ class MethodChannelLocation extends LocationPlatform {
 
   /// Enables or disables service in the background mode.
   @override
-  Future<bool> enableBackgroundMode({bool? enable}) async {
+  Future<bool> enableBackgroundMode({
+    bool? enable,
+    bool? isForeground = false,
+  }) async {
     final result = await _methodChannel!.invokeMethod(
       'enableBackgroundMode',
-      <String, dynamic>{'enable': enable},
+      <String, dynamic>{'enable': enable, 'foreground': isForeground},
     );
 
     return result == 1;
